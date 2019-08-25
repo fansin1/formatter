632cc6b1f631dc1ad175ca336ba03fd2bad2141e
fansin1 <artem21481@gmail.com>
2019-08-25 18:45:30 +0500

    1. Refactored TableFormatter and EmptyBorderFormatter. Now tabulation is part of EmptyBorderFormatter. 2. Added AsciiBorderFormatter. It make table without '\t', but with with [-, |, +]. 3. Remake tests and add new for AsciiBorderFormatter

--- src/main/kotlin/formatter/TableFormatter.kt
+++ src/main/kotlin/formatter/TableFormatter.kt
@@ -3,16 +3,16 @@ package formatter
 class TableFormatter(private val borderFormatter: BorderFormatter) {
 
     companion object {
-        fun create(): TableFormatter {
-            return TableFormatter(EmptyBorderFormatter())
+        fun create(borderFormatter: BorderFormatter = EmptyBorderFormatter()): TableFormatter {
+            return TableFormatter(borderFormatter)
         }
     }
 
-    private val rows = mutableListOf<List<String>>()
+    private val rows = mutableListOf<MutableList<String>>()
 
 
     fun<T> addRow(vararg values: T) {
-        rows.add(values.asSequence().map { it.toString() }.toList())
+        rows.add(values.asSequence().map { it.toString() }.toMutableList())
     }
 
     fun removeAt(index: Int) {
@@ -31,9 +31,16 @@ class TableFormatter(private val borderFormatter: BorderFormatter) {
             }.max() ?: 0
         }
 
-        val res: Sequence<List<String>> = rows.asSequence().map {
+        val sameSizeRows = rows.map { row ->
+            while (row.size < columnsCount) {
+                row.add("")
+            }
+            row
+        }
+
+        val res: Sequence<List<String>> = sameSizeRows.asSequence().map {
             row -> row.mapIndexed {
-                index, elem -> elem + " ".repeat(lengths[index] - elem.length) + "\t"
+                index, elem -> elem + " ".repeat(lengths[index] - elem.length)
             }
         }
 
